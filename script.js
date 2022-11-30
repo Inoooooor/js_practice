90b8f1053c50e334bba0fcde47a93b332fd48751
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2022-11-30 18:18:44 +0300

    optimized_code

--- script.js
+++ script.js
@@ -1,66 +1,50 @@
 let scoreNum = 0;
 
 function myFunction(x) {
-    x.classList.toggle("change");
-  }
+  x.classList.toggle("change");
+}
 
-  function scorePlus() {
-    scoreNum++;
-    document.querySelector('.count_num').innerHTML = scoreNum;
-  }
+function scorePlus() {
+  scoreNum++;
+  document.querySelector('.count_num').innerHTML = scoreNum;
+}
 
-  function scoreMinus() {
-    scoreNum--;
-    document.querySelector('.count_num').innerHTML = scoreNum;
-  }
+function scoreMinus() {
+  scoreNum--;
+  document.querySelector('.count_num').innerHTML = scoreNum;
+}
 
 function opponentsChoice() {
-    let numForChoice = 0;
-    let opponentsPlay;
-    numForChoice = Math.floor((Math.random() * 10)) % 3;
-    switch(numForChoice) {
-      case 0:
-        opponentsPlay = "rock";
-        break;
-      case 1:
-        opponentsPlay = "paper";
-        break;
-      case 2:
-        opponentsPlay = "scissors";
-        break;
-        default:
-        break;
-    }
-    document.querySelector('.rand').innerHTML = opponentsPlay;
-    return opponentsPlay;
+  let numForChoice = Math.floor((Math.random() * 10)) % 3;
+  const opponentsArr = ["rock", "paper", "scissors"];
+  document.querySelector('.rand').innerHTML = opponentsArr[numForChoice];
+  return opponentsArr[numForChoice];
 }
 
 function playFunc(playersChoice) {
   const win = "You win", lose = "You lose", tie = "It's a tie";
   const rock = "rock", paper = "paper", scissors = "scissors";
-  let wonOrLost = ".won_lost";
+  let wonOrLost = document.querySelector(".won_lost");
   let opponentChoiceVar = opponentsChoice();
-  // console.log(playersChoice.id);
-  // console.log(opponentChoiceVar);
   if (playersChoice.id == opponentChoiceVar) {
-    document.querySelector(wonOrLost).innerHTML = tie;
+    wonOrLost.innerHTML = tie;
   } else if (playersChoice.id == rock && opponentChoiceVar == paper) {
-      document.querySelector(wonOrLost).innerHTML = lose;
+      wonOrLost.innerHTML = lose;
       scoreMinus();
   } else if (playersChoice.id == rock && opponentChoiceVar == scissors) {
-      document.querySelector(wonOrLost).innerHTML = win;
+      wonOrLost.innerHTML = win;
       scorePlus();
   } else  if (playersChoice.id == paper && opponentChoiceVar == rock) {
-      document.querySelector(wonOrLost).innerHTML = win;
+      wonOrLost.innerHTML = win;
       scorePlus();
   } else if (playersChoice.id == paper && opponentChoiceVar == scissors) {
-      document.querySelector(wonOrLost).innerHTML = lose;
+      wonOrLost.innerHTML = lose;
       scoreMinus();
   } else if (playersChoice.id == scissors && opponentChoiceVar == rock) {
-      document.querySelector(wonOrLost).innerHTML = lose;
+      wonOrLost.innerHTML = lose;
       scoreMinus();
   } else if (playersChoice.id == scissors && opponentChoiceVar == paper) {
-      document.querySelector(wonOrLost).innerHTML = win;
+      wonOrLost.innerHTML = win;
       scorePlus();
   }
 
